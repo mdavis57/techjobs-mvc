cdeae37b001349c4291b7503a7490f9a74204917
Milena Davis <nilenadavis8808@gmail.com>
2019-06-10 16:16:39 -0500

    Finished project (no bonus missions)

--- src/main/java/org/launchcode/controllers/SearchController.java
+++ src/main/java/org/launchcode/controllers/SearchController.java
@@ -24,21 +24,26 @@ public class SearchController {
 
     // TODO #1 - Create handler to process search request and display results
 
-//reuse code from jobdata
-//parameters - sting column, string searchTerm,
+
     @RequestMapping(value = "results")
     public String results(Model model, @RequestParam String searchType, @RequestParam String searchTerm) {
         model.addAttribute("columns", ListController.columnChoices);
 
-
+        //if you want to search through all of the columnChoices by a certain search term
         if (searchType.equals("all")) {
+
+            //initialize result variable
             ArrayList<HashMap<String, String>> jobs = JobData.findByValue(searchTerm);
+
+            //create variable for result size to use in template
             String result = jobs.size() + " Result(s)";
             model.addAttribute("jobs", jobs);
             model.addAttribute("searchTerm", searchTerm);
             model.addAttribute("result", result);
 
         }
+
+        //searches for given search term through collumn of choice (searchType)
         else {
             ArrayList<HashMap<String, String>> jobs = JobData.findByColumnAndValue(searchType, searchTerm);
             String result = jobs.size() + " Result(s)";
@@ -48,6 +53,7 @@ public class SearchController {
 
         }
 
+        //renders the search.html template
         return "search";
 
     }
